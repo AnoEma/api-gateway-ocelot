7b9c4fbe2bc103f25d9b83ee2ba0c137b8780cda
anoema <anoemangi@gmail.com>
2023-10-30 12:38:44 -0300

    Add Ocelot Cache and finish de project

--- OrderProcessing/Program.cs
+++ OrderProcessing/Program.cs
@@ -1,3 +1,4 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -5,12 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddOcelot(builder.Configuration)
+                .AddCacheManager(x => { x.WithDictionaryHandle(); });
 
 var app = builder.Build();
 
 await app.UseOcelot();
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
